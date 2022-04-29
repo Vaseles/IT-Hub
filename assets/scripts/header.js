2079c0aa3081f39441944cb18ca5ca51d969433d
Vaseles <vaselesstolarov813@gmail.com>
2022-04-30 00:48:19 +0600

    1.3

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -8,7 +8,6 @@ HeaderOpen.addEventListener("click", function() {
 });
 HeaderClose.addEventListener("click", function() {
     menu.style.bottom = "100%";
-    menu.style.backgroundImage = `none`;
     clear();
 });
 
@@ -24,6 +23,8 @@ const colledgeParamsAbout = document.querySelector(".colledge-params--about");
 const colledgeParamsnNotcorruptin = document.querySelector(".colledge-params--notcorruptin");
 const colledgeNotcorruptin = document.querySelector(".colledge--notcorruptin")
 
+const AllMenuLi = [colledgeAbout, colledgeNotcorruptin]
+
 //function
 //for error
 function close() {
@@ -31,15 +32,26 @@ function close() {
         i.style.display = "none";
     }
 }
+function del() {
+    for (let i of AllMenuLi) {
+        i.classList.remove("norm")
+    }
+}
 
 //go
 colledgeAbout.addEventListener("click", function() {
     close();
+    del();
+    
     colledgeParamsAbout.style.display = "flex";
     menu.style.backgroundImage = `url("https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")`;
+    colledgeAbout.classList.add("norm");
 });
 colledgeNotcorruptin.addEventListener("click", function() {
-    close();
+    close()
+    del()
+
     colledgeParamsnNotcorruptin.style.display = "flex";
     menu.style.backgroundImage = `url("https://images.pexels.com/photos/1806900/pexels-photo-1806900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")`;
+    colledgeNotcorruptin.classList.add("norm");
 })
